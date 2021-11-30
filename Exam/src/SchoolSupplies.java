e71edff163c4f628fa17acddb321bdaf988cc4e1
Evgeni Genov <43596303+Evgeni-Genov@users.noreply.github.com>
2021-11-30 17:44:37 +0200

    Add files via upload

---
+++ Exam/src/SchoolSupplies.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SchoolSupplies {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int pens = Integer.parseInt(scanner.nextLine());
+        int markers = Integer.parseInt(scanner.nextLine());
+        double litersCleaner = Double.parseDouble(scanner.nextLine());
+        int discount = Integer.parseInt(scanner.nextLine());
+
+        double pricePens = pens * 5.80;
+        double priceMarkers = markers * 7.20;
+        double priceCleaner = litersCleaner * 1.20;
+        double totalPrice = priceCleaner + priceMarkers + pricePens;
+        double totalPriceWithDiscount = totalPrice - (discount * 1.0 / 100) * totalPrice;
+
+        System.out.printf("%.3f", totalPriceWithDiscount);
+    }
+}
---
+++ Exam/src/caloriesFromWalking.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class caloriesFromWalking {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int minutesWalk = Integer.parseInt(scanner.nextLine());
+        int walks = Integer.parseInt(scanner.nextLine());
+        int calories = Integer.parseInt(scanner.nextLine());
+
+        int total = minutesWalk * walks * 5;
+        double neededCalories = 0.5 * calories;
+
+        if (total >= neededCalories) {
+            System.out.printf("Yes, the walk for your cat is enough. Burned calories per day: %d.",
+                    total);
+
+        } else {
+            System.out.printf("No, the walk for your cat is not enough. Burned calories per day: %d.",
+                    total);
+        }
+    }
+}
